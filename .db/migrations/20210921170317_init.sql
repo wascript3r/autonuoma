57ced10802f6dd73b480e5269347f86f65d9bf8e
WaScript3r <wascript3r@gmail.com>
2021-09-21 22:15:30 +0300

    updated db schema

--- .db/migrations/20210921170317_init.sql
+++ .db/migrations/20210921170317_init.sql
@@ -6,7 +6,7 @@
 --
 
 -- Dumped from database version 12.1 (Debian 12.1-1.pgdg100+1)
--- Dumped by pg_dump version 12.2
+-- Dumped by pg_dump version 12.8
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -20,6 +20,36 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 SET default_table_access_method = heap;
+
+--
+-- Name: roles; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.roles (
+    id integer NOT NULL,
+    role character varying(20) NOT NULL
+);
+
+
+--
+-- Name: roles_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.roles_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: roles_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.roles_id_seq OWNED BY public.roles.id;
+
 
 --
 -- Name: sessions; Type: TABLE; Schema: public; Owner: -
@@ -40,7 +70,8 @@ CREATE TABLE public.users (
     id integer NOT NULL,
     email character varying(200) NOT NULL,
     password character varying(70) NOT NULL,
-    balance bigint DEFAULT 0 NOT NULL
+    balance bigint DEFAULT 0 NOT NULL,
+    role_id integer DEFAULT 1 NOT NULL
 );
 
 
@@ -65,10 +96,59 @@ ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
 
 --
+-- Name: roles id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.roles ALTER COLUMN id SET DEFAULT nextval('public.roles_id_seq'::regclass);
+
+
+--
 -- Name: users id; Type: DEFAULT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- Data for Name: roles; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+COPY public.roles (id, role) FROM stdin;
+1	User
+2	Support agent
+3	Administrator
+\.
+
+
+--
+-- Data for Name: sessions; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+COPY public.sessions (id, user_id, expiration) FROM stdin;
+\.
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+COPY public.users (id, email, password, balance, role_id) FROM stdin;
+\.
+
+
+--
+-- Name: roles_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.roles_id_seq', 3, true);
+
+
+--
+-- Name: roles roles_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.roles
+    ADD CONSTRAINT roles_pkey PRIMARY KEY (id);
 
 
 --
@@ -111,11 +191,26 @@ CREATE INDEX fki_sessions_fkey ON public.sessions USING btree (user_id);
 
 
 --
+-- Name: fki_users_fkey; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX fki_users_fkey ON public.users USING btree (role_id);
+
+
+--
 -- Name: sessions sessions_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.sessions
     ADD CONSTRAINT sessions_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) NOT VALID;
+
+
+--
+-- Name: users users_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_fkey FOREIGN KEY (role_id) REFERENCES public.roles(id) NOT VALID;
 
 
 --
